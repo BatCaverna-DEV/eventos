66c5f20d633456cfcb78cdd3905ac125a5a7c98c
Bruno Vicente <brunnovicente@gmail.com>
2025-11-17 01:08:56 -0300

    Correções
    Pequenas correções

--- eventos-front/src/views/Presenca.vue
+++ eventos-front/src/views/Presenca.vue
@@ -5,21 +5,32 @@ import {ref, onMounted} from "vue";
 
 const atividades = ref({})
 const API = import.meta.env.VITE_API_URL
+const url = import.meta.env.BASE_URL
+let carregando = ref(true)
 
 onMounted(async () => {
   const resposta = await fetch(`${API}/presenca/atividades`)
   atividades.value = await resposta.json()
+  carregando.value = false
 })
 
 </script>
 
 <template>
   <NavExt></NavExt>
-  <div class="container-sm d-flex flex-wrap gap-3 justify-content-between">
+
+  <div v-if="carregando" class="d-flex justify-content-center">
+    <div class="spinner-border text-success my-3" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+
+
+  <div v-if="!carregando" class="container-sm d-flex flex-wrap gap-3 justify-content-between">
       <div class="card" style="width: 13rem;" v-for="atividade in atividades">
         <img :src="`${atividade.imagem}`" class="card-img-bottom" alt="">
         <div class="card-body">
-          <a :href="`/registrar/${atividade.id}`" class="btn btn-primary">Registrar</a>
+          <a :href="`${url}registrar/${atividade.id}`" class="btn btn-primary">Registrar</a>
         </div>
       </div>
   </div>
--- eventos-front/src/views/Registrar.vue
+++ eventos-front/src/views/Registrar.vue
@@ -10,6 +10,9 @@ import {useRoute, useRouter} from "vue-router";
 
   const permitido = ref(false)
   const API = import.meta.env.VITE_API_URL
+  let carregando = ref(true)
+  let erro = ref(false)
+  let mensagem = ref('')
 
   const atividade = ref({})
   const presenca = ref({nome: '', matricula:'', turma:'', atividade_id:0})
@@ -17,6 +20,7 @@ import {useRoute, useRouter} from "vue-router";
   onMounted(async () => {
     const resposta = await fetch(`${API}/presenca/atividade/`+id)
     atividade.value = await resposta.json()
+    carregando.value = false
   })
 
   async function verificarIp() {
@@ -31,7 +35,8 @@ import {useRoute, useRouter} from "vue-router";
   }
 
   async function salvar() {
-
+    carregando.value = true
+    erro.value = false
     const ok = await verificarIp()
     if(!ok){
       alert('Você precisa está na Rede Wi-Fi do IFMA!')
@@ -52,17 +57,31 @@ import {useRoute, useRouter} from "vue-router";
     if(resposta.ok){
       if(resposta.status === 500){
         const msg = await resposta.json()
-        alert(msg.message)
+        carregando.value = false
+        erro.value = true
+        mensagem.value = msg.message
       }else{
         alert('Presença registrada com sucesso!')
         router.push('/')
       }
     }else{
-      const erro = await resposta.json()
-      alert('ERRO: '+erro.message)
+      const error = await resposta.json()
+      carregando.value = false
+      erro.value = true
+      mensagem.value = error.message
     }
 
   }
+
+  function formatarData(dataISO) {
+    const data = new Date(dataISO)
+
+    return data.toLocaleString('pt-BR', {
+      dateStyle: 'short',
+      timeStyle: 'short'
+    })
+  }
+
 </script>
 
 <template>
@@ -71,10 +90,22 @@ import {useRoute, useRouter} from "vue-router";
     <nav class="navbar bg-body-tertiary">
       <div class="container-fluid">
         <span class="h5">Registrar Presença</span>
-        <a class="btn btn-secondary" href="/">Voltar</a>
+        <RouterLink to="/" class="btn btn-secondary">Voltar</RouterLink>
       </div>
     </nav>
-    <div class="row">
+
+    <div v-if="carregando" class="d-flex justify-content-center">
+      <div class="spinner-border text-success my-3" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+
+    <div v-if="erro" class="alert alert-danger alert-dismissible fade show" role="alert">
+      <strong>ATENÇÃO: </strong> {{ mensagem }}
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+
+     <div v-if="!carregando" class="row">
       <div class="col-sm">
         <label for="descricao">ATIVIDADE</label>
         <div class="bg-body-secondary">
@@ -82,7 +113,24 @@ import {useRoute, useRouter} from "vue-router";
         </div>
       </div>
     </div>
-    <form @submit.prevent="salvar">
+
+    <div v-if="!carregando" class="row">
+      <div class="col-sm-3">
+        <label for="descricao">INÍCIO</label>
+        <div class="bg-body-secondary">
+          <input :value="formatarData(atividade.inicio)" type="text" class="form-control" disabled>
+        </div>
+      </div>
+      <div class="col-sm-3">
+        <label for="descricao">FIM</label>
+        <div class="bg-body-secondary">
+          <input :value="formatarData(atividade.final)" type="text" class="form-control" disabled>
+        </div>
+      </div>
+    </div>
+
+    <form v-if="!carregando" class="mt-3" @submit.prevent="salvar">
+      <h5 class="bg-body-secondary p-2">Dados do Aluno</h5>
       <div class="row mt-3">
         <div class="col-sm-3">
           <label for="matricula">MATRÍCULA</label>
@@ -108,6 +156,13 @@ import {useRoute, useRouter} from "vue-router";
             <option value="2ADM - Vespertino">2ADM - Vespertino</option>
             <option value="3TI A - Vespertino">3TI A - Vespertino</option>
             <option value="3TI B - Vespertino">3TI B - Vespertino</option>
+            <option value="ADS - Módulo IV">ADS - Módulo IV</option>
+            <option value="ADS - Módulo V">ADS - Módulo V</option>
+            <option value="ADM - Módulo VI">ADM - Módulo VI</option>
+            <option value="Proeja">Proeja</option>
+            <option value="Automação - Módulo III">Automação - Módulo III</option>
+            <option value="Automação - Módulo IV">Automação - Módulo IV</option>
+            <option value="Automação - Módulo IV">Partiu IF</option>
           </select>
         </div>
       </div>
